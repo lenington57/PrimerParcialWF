a6ad5a087def7d923e1e16623efe6b98beb0136e
Silvestre <Silvestre@LAPTOP-2R90FK4G>
2019-02-02 16:07:09 -0400

    Incluyendo Clases.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<CuentaBancaria> CuentaBancaria { get; set; }
+
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
